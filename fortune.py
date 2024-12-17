c8c975e1e8bfd446b495cf7da0696ff654e39a1a
Nacroni <thespokenone209@hotmail.com>
2024-12-17 09:38:13 -0500

    add more verbose prints

--- fortune.py
+++ fortune.py
@@ -6,12 +6,14 @@ parser.add_argument('-v', '--verbose', help='not a good verbose mode', action='s
 args = parser.parse_args()
 verbose_enable = args.verbose
 
+if verbose_enable:print('Checking ./fortune/ directory...')
 file_list = os.listdir('./fortune/')
-chosen_file = random.choice(file_list)
 
-if verbose_enable: print(f'Chosen File: {chosen_file}')
+chosen_file = random.choice(file_list)
+if verbose_enable:print(f'Chosen File: {chosen_file}')
 
 if chosen_file.startswith('.'):
+    if verbose_enable: print('File is hidden, restarting...')
     os.execv(sys.argv[0], sys.argv)
 
 file = open(f'./fortune/{chosen_file}', 'r')
